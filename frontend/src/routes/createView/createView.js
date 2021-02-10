adc857415fa76cee44c056f725b33d734164ddd7
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2021-02-10 12:27:49 -0300

    form for new order request completed

--- frontend/src/routes/createView/createView.js
+++ frontend/src/routes/createView/createView.js
@@ -1,19 +1,22 @@
 import React, {Component, useState} from 'react';
 import {connect} from 'react-redux';
-import {Form, Button, FormFile} from 'react-bootstrap';
+import {Form, Button, Alert, Modal} from 'react-bootstrap';
 import toBase64file from './convertToBase64'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../sharedStyles.css';
-import { addToList } from "../../actionsCreators";
+import {addToList} from "../../actionsCreators";
+import {Link} from "react-router-dom";
 
 class CreateView extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            spin: false,
+            alert: false,
+            success: false,
             subject: '',
             body: '',
-            file: '',
-            spin: false
+            code: ''
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -31,54 +34,110 @@ class CreateView extends Component {
 
     async handleSubmit(event) {
         event.preventDefault();
-        await this.setState({
-            spin: true
-        })
-        console.log(this.state.spin)
-        const file = event.target["exampleFormControlFile1"].files[0]
 
-        const fileToBase64 = await toBase64file(file);
-        const subject = this.state.subject;
-        const body = this.state.body;
-        await fetch('http://localhost:3001/order-request/', {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify({
-                subject: subject,
-                body: body,
-                file: fileToBase64
+        if ((this.state.subject == '') || (this.state.body == '')) {
+            this.setState({
+                alert: true
             })
-        }).then((res) => {
-            console.log("data received");
-            return res.json()
-        })
-            .then(res => {
-                this.props.addToList(res.orderRequest)
+        } else {
+            console.log("entrada de form ")
+            let file;
+            let fileName;
+            let fileToBase64;
+            if (event.target["exampleFormControlFile1"].files[0] != undefined) {
+                file = event.target["exampleFormControlFile1"].files[0]
+                console.log(file);
+                fileToBase64 = await toBase64file(file);
+                fileName = file.name;
+                console.log(fileName)
+            } else {
+                file = null;
+                fileName = null;
+            }
+            const subject = this.state.subject;
+            const body = this.state.body;
+            await fetch('http://localhost:3001/order-request/', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    subject: subject,
+                    body: body,
+                    file: fileToBase64,
+                    filename: fileName
+                })
+            }).then((res) => {
+                console.log("status: ", res.status, res.status in [200, 201])
+                if (res.status == 200) {
+                    return Promise.resolve(res.json())
+                } else {
+                    return Promise.reject(res.json())
+                }
 
-                return res.orderRequest
             })
-            .catch(err => {
-                console.log(err)
-            })
-        //window.location.replace("/");
+                .then(res => {
+                    this.setState({
+                        success: true,
+                        code: res.orderRequest.code
+                    });
+                    this.props.addToList(res.orderRequest)
 
+                    return res.orderRequest
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+        }
     }
+
+    setShowError() {
+        this.setState({
+            alert: false
+        })
+    }
+
     render() {
         return (
             <div id="container">
                 <Form onSubmit={this.handleSubmit}>
+                    {[this.state.alert].map((alert) => {
+                        if (alert) {
+                            return (
+                                <Alert variant={"danger"} onClose={() => this.setShowError()} dismissible>Subject and
+                                    Body can't be blank</Alert>)
+                        }
+                    })}
                     <Form.Group controlId="formGroupEmail">
                         <Form.Label>Subject :</Form.Label>
                         <Form.Control name="subject" type="text" value={this.state.subject}
                                       onChange={this.handleChange}/>
                         <Form.Label>Body: </Form.Label>
-                        <Form.Control name="body" type="text" value={this.state.body} onChange={this.handleChange}/>
+                        <Form.Control name="body" type="text" value={this.state.body} as="textarea" rows={3}
+                                      onChange={this.handleChange}/>
                         <Form.File id="exampleFormControlFile1" label="File input"/>
                     </Form.Group>
                     <Button type="submit" value="Submit">Submit</Button>
                 </Form>
+                <Modal
+                    show={this.state.success}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header>
+                        <Modal.Title>Order Request Sent</Modal.Title>
+                    </Modal.Header>
+                    {[this.state.code].map(code => {
+                        return (
+                            <Modal.Body> Your code confirm is {code}
+                            </Modal.Body>
+                        )
+                    })}
+
+                    <Modal.Footer>
+                        <Link to="/"><Button variant="primary">Home</Button></Link>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }
--- frontend/src/routes/orderRequestView/lookUp/lookUp.js
+++ frontend/src/routes/orderRequestView/lookUp/lookUp.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { search } from "../../../actionsCreators";
-import {Button, Navbar, NavDropdown, Nav, Form, FormControl} from 'react-bootstrap';
+import { Navbar, Form } from 'react-bootstrap';
 
 class LookUp extends Component {
     constructor(props) {
@@ -26,23 +26,10 @@ class LookUp extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
                     <Form inline>
                         <Form.Control type="text" placeholder="Search" className="mr-sm-2" value={this.props.index} onChange={this.handleChange}/>
-                        <Button variant="outline-success">Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
